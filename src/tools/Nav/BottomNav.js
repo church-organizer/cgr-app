a21b9afc3489f8ab39405898900c9a5b5c7c144f
nloetkemann <n.loetkemann@mittwald.de>
2019-08-16 14:09:43 +0200

    bottom finished

--- src/tools/Nav/BottomNav.js
+++ src/tools/Nav/BottomNav.js
@@ -7,40 +7,41 @@ import StarRateIcon from '@material-ui/icons/StarRate';
 import useMediaQuery from "@material-ui/core/useMediaQuery/useMediaQuery";
 import {makeStyles,} from "@material-ui/core";
 import {Link} from "react-router-dom";
-import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
         width: "100%",
         position: "fixed",
         bottom: 0,
-        height: "50px",
+        height: "70px",
         borderTop: "1px solid #eee",
         zIndex: 1202,
         backgroundColor: "#eee",
         boxShadow: "0px 4px 20px 0px rgba(0,0,0,0.75)"
     },
     onActive: {
-        color: "#4b64ee"
+        color: "#3f51b5",
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
     },
     link: {
         width: "30%"
     }
-});
+}));
 
 const BottomNav = (props) => {
     const classes = useStyles();
-    const [value, setValue] = React.useState('');
+    const [value, setValue] = React.useState(0);
     const matches = useMediaQuery('(min-width:1100px)');
     
-    // const path= props.pathname.split("/")[1];
-    
+
 
     const onClick = (newValue) => {
         setValue(newValue)
@@ -53,19 +54,20 @@ const BottomNav = (props) => {
         <Grid className={classes.root} container justify={"space-evenly"}>
             <Grid item xs={12}>
                 <Tabs indicatorColor="primary" textColor="secondary" value={value} aria-label="icon label tabs example">
-                    <Grid item xs>
-                        <Link onClick={() => onClick(0)} to={"/wiki"}>
-                            <Tab classes={value=== 0? {labelIcon: classes.active} : {}} icon={<PanoramaIcon/>} aria-label="wiki" label={"Wiki"}/>
+                    <Tab style={{display: "none"}}/>
+                    <Grid item xs={4}>
+                        <Link onClick={() => onClick(1)} to={"/wiki"}>
+                            <Tab classes={value=== 1? {labelIcon: classes.onActive} : {}} icon={<BookIcon/>} aria-label="wiki" label={"Wiki"}/>
                         </Link>
                     </Grid>
-                    <Grid item xs>
-                        <Link onClick={() => onClick(1)} to={"/rating"}>
-                            <Tab icon={<PanoramaIcon/>} aria-label="rating" label={"Rating"}/>
+                    <Grid item xs={4}>
+                        <Link onClick={() => onClick(2)} to={"/rating"}>
+                            <Tab classes={value=== 2? {labelIcon: classes.onActive} : {}} icon={<StarRateIcon/>} aria-label="rating" label={"Rating"}/>
                         </Link>
                     </Grid>
-                    <Grid item xs>
-                        <Link onClick={() => onClick(2)} to={"/media"}>
-                            <Tab icon={<PanoramaIcon/>} aria-label="Media" label={"Medien"}/>
+                    <Grid item xs={4}>
+                        <Link onClick={() => onClick(3)} to={"/media"}>
+                            <Tab classes={value=== 3? {labelIcon: classes.onActive} : {}} icon={<PanoramaIcon/>} aria-label="Media" label={"Medien"}/>
                         </Link>
                     </Grid>
                 </Tabs>
