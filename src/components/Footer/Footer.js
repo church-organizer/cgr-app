efff188082ffc770f085ec2d5bf2b3ef74a314ff
nloetkemann <n.loetkemann@mittwald.de>
2019-08-30 15:49:08 +0200

    footer new

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {makeStyles} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+
+const useStyle = makeStyles(theme => ({
+    root: {
+        position: "relative",
+        bottom: 0,
+        right: 0,
+        height: "40px",
+        borderTop: "1px solid",
+        borderColor: "rgba(120,120,120,0.33)",
+        backgroundColor: "white",
+        marginTop: "100px",
+        padding: "20px",
+    }
+}));
+
+
+const Footer = (props) => {
+    const classes = useStyle();
+    return (
+        <div className={classes.root} >
+            <Grid container direction={"row"} justify={"space-around"} alignItems={"center"}>
+                <Grid item xs>Made with Heart</Grid>
+                <Grid item xs>Footer</Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default Footer;
