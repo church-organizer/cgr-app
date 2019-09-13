71ef359abc83faedb6b57fd7620ac2343e63bd6b
nloetkemann <n.loetkemann@mittwald.de>
2019-09-13 07:25:50 +0200

    correct height

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -20,7 +20,7 @@ class Page extends Component {
     render() {
         return (
             <Fade in={true} timeout={0.6}>
-                <div id="page-content" style={{minHeight : "calc(100vh - 120px)"}}>
+                <div id="page-content" style={{minHeight : "calc(100vh - 110px)"}}>
                     <Content readOnly={this.props.readOnly} title={this.props.filename} content={this.state.content}/>
                 </div>
             </Fade>
