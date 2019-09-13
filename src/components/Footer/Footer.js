c2c8dd48692efd8902831c33fd13d548b9e4a8a9
nloetkemann <n.loetkemann@mittwald.de>
2019-09-13 13:48:37 +0200

    footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import {makeStyles} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
-const useStyle = makeStyles(theme => ({
-    root: {
-        borderTop: "1px solid",
-        borderColor: "rgba(120,120,120,0.33)",
-        backgroundColor: "white",
-        padding: "15px",
-        zIndex: -10
-    }
-}));
-
-
-const Footer = (props) => {
-    const classes = useStyle();
+const Footer = () => {
     return (
-        <div className={classes.root} >
+        <div className="footer" >
             <Grid container direction={"row"}>
                 <Grid item xs>Made with Heart</Grid>
                 <Grid item xs>Footer</Grid>
