9dbf259ecccf193b51a8bb2effc3a3ae2365f1b0
nloetkemann <n.loetkemann@mittwald.de>
2019-09-18 08:45:13 +0200

    not working with searching api

--- src/services/SearchContent.js
+++ src/services/SearchContent.js
@@ -1,16 +1,16 @@
 const changeContentIfMatch = (content, searchContent) => {
-    // return content;
-    const regex = new RegExp(searchContent, "gi");
-    let matches = content.match(regex);
-
-
-    if (matches && searchContent) {
-        for (let match of matches) {
-            const regex = new RegExp(match, "g");
-            content = content.replace(regex, `<span class="match">${match}</span>`);
-        }
-    }
     return content;
+    // const regex = new RegExp(searchContent, "gi");
+    // let matches = content.match(regex);
+    //
+    //
+    // if (matches && searchContent) {
+    //     for (let match of matches) {
+    //         const regex = new RegExp(match, "g");
+    //         content = content.replace(regex, `<span class="match">${match}</span>`);
+    //     }
+    // }
+    // return content;
 };
 
 export default changeContentIfMatch;
