65583297bff222d86057ed61989cbfdc80f407a7
nloetkemann <n.loetkemann@mittwald.de>
2019-09-25 16:07:57 +0200

    make image links work

--- src/components/Page/Markdown.js
+++ src/components/Page/Markdown.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactHtmlParser from 'react-html-parser';
 import marked from "marked";
 import "./Markdown.css"
@@ -7,7 +7,7 @@ import FileLoader from "../../services/FileLoader";
 const Markdown = (props) => {
     let content = props.source;
     const hashtagRegex = /\B#[\w\-\.\_]+(?:\s|$)/g;
-    const imageRegex = /\!\[.*\]\(.*.png\)/g;
+    const imageRegex = /\!\[.*\]\(.*.(png|jpg|jpeg|gif)\)/g;
 
 
     const changeHashtagIntoLink = () => {
@@ -25,17 +25,17 @@ const Markdown = (props) => {
     const changeImageUrlInMD = () => {
         const logoRegex = /\(.*\)/;
         const matches = content.match(imageRegex);
+        console.log(matches);
         if(matches) {
             for (let match of matches) {
                 let newUrl = match;
                 let imageName = newUrl.match(logoRegex)[0];
                 imageName = imageName.replace(/(\(|\))/g, "");
-                newUrl = newUrl.replace(logoRegex, "("+FileLoader.url + "images/" + imageName + ")");
+                newUrl = newUrl.replace(logoRegex, "("+FileLoader.url + "image/" + imageName + ")");
                 content = content.replace(match, newUrl);
             }
         }
     };
-
     if(content){
         changeHashtagIntoLink();
         changeImageUrlInMD();
