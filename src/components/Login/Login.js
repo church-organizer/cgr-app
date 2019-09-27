cf560089706d1fcb9c54e79a975095c160d82f90
nloetkemann <n.loetkemann@mittwald.de>
2019-09-27 07:50:52 +0200

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {
     Dialog, DialogActions, DialogContent,
     DialogContentText,
@@ -7,6 +7,7 @@ import {
 } from "@material-ui/core";
 import './Login.css'
 import Button from "@material-ui/core/Button";
+import FileLoader from "../../services/FileLoader";
 
 
 /**
@@ -18,10 +19,13 @@ import Button from "@material-ui/core/Button";
  */
 const Login = (props) => {
     const [user, setUser] = React.useState({userName: '', password: ''});
-    const loginUrl = "https://churchtools.cg-rahden.de/index.php?q=login/ajax";
-    const [loggedIn, setLoggedIn] = useState(false);
 
 
+    /**
+     * gets called if the input fields changes
+     * @param value the changed value
+     * @param type "name" or "password"
+     */
     const onChange = (value, type) => {
         if (type === "name") {
             setUser({userName: value, password: user.password});
@@ -30,32 +34,47 @@ const Login = (props) => {
         }
     };
 
+    /**
+     * loads the jwt from cache
+     * so the user doesnt have to log in every time
+     */
     const loadFromCache = () => {
+
+    };
+
+    /**
+     * saves the token in the cache
+     * @param token
+     */
+    const saveInCache = (token) => {
 
     };
 
 
     const login = () => {
-        fetch(loginUrl, {
+        fetch(FileLoader.url + "login", {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: [JSON.stringify({
-                func: 'login',
+            body: JSON.stringify({
                 email: user.userName,
                 password: user.password
-            })]
+            })
         }).then(res => {
             console.log(res);
+            if (res.status === 200) {
+                props.onSuccess();
+            }
+            // props.onSuccess();
         }).catch(error => {
             console.error(error);
         });
     };
 
     return (
-        <Dialog open={props.open}>
+        <Dialog open={props.open} onBackdropClick={() => props.onAbort()} onEscapeKeyDown={()=> props.onAbort()}>
             <DialogTitle>Login</DialogTitle>
             <DialogContent>
                 <DialogContentText>Bitte logge dich erstmal ein.</DialogContentText>
@@ -70,10 +89,12 @@ const Login = (props) => {
                 </div>
             </DialogContent>
             <DialogActions>
-                <Button type={"submit"} onClick={() => {
-                    login()
-                }} variant={"outlined"} color={"secondary"}>Abbrechen</Button>
-                <Button variant={"outlined"} color={"primary"}>Login</Button>
+                <Button variant={"outlined"} color={"secondary"} onClick={() => props.onAbort()}>Abbrechen</Button>
+                <Button variant={"outlined"}
+                        color={"primary"}
+                        onClick={() => login()}>
+                    Login
+                </Button>
             </DialogActions>
         </Dialog>
     );
